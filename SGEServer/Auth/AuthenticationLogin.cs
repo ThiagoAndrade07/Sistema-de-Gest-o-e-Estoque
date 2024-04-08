648d221635f7e797f07b32e1328114e5d5f68f7c
ThiagoAndrade07 <120478086+ThiagoAndrade07@users.noreply.github.com>
2024-04-08 18:33:27 -0300

    Add files via upload

---
+++ SGEServer/Auth/AuthenticationLogin.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.Security.Claims;
+using SGEServer.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace SGEServer.Auth
+{
+    public class AuthenticationLogin : AuthenticationStateProvider
+    {
+        public static readonly string tokenKey = "SGEAPP";
+        private readonly IJSRuntime _js;
+        private readonly IConfiguration _config;
+
+        public AuthenticationLogin(IJSRuntime js, IConfiguration config)
+        {
+            _js = js;
+            _config = config;
+        }
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                string token;
+
+                token = await _js.GetFromLocalStorage(tokenKey);
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    return NotAuthenticate;
+                }
+
+                return CreateAuthenticationState(token);
+            }
+            catch
+            {
+                return NotAuthenticate;
+            }
+        }
+
+        public async Task<AuthenticationState> ValidaSessao()
+        {
+
+            return await GetAuthenticationStateAsync();
+        }
+
+        public async Task Login(string token)
+        {
+            try
+            {
+                await _js.SetInLocalStorage(tokenKey, token);
+
+                var authState = CreateAuthenticationState(token);
+
+                NotifyAuthenticationStateChanged(Task.FromResult(authState));
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task Logout()
+        {
+            try
+            {
+                await _js.RemoveItem(tokenKey);
+                NotifyAuthenticationStateChanged(Task.FromResult(NotAuthenticate));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private static AuthenticationState NotAuthenticate =>
+            new(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        public AuthenticationState CreateAuthenticationState(string token)
+        {
+            return new AuthenticationState(new ClaimsPrincipal
+                (new ClaimsIdentity(new TokenService(_config).GetClaims(token), "jwt")));
+        }
+
+        public static async Task<bool> AuthenticateUser(AuthenticationLogin AuthState, LoginModel Usr, NavigationManager Navigation)
+        {
+            bool isAuthenticated = false;
+
+            var Auth = await AuthState.GetAuthenticationStateAsync();
+
+            if (Auth.User.Identity != null && Auth.User.Identity.IsAuthenticated)
+            {
+                Usr.CodUsuario = int.Parse(Auth.User.FindFirst("UserId")?.Value ?? "0");
+                Usr.Usuario = Auth.User.FindFirst("UserLogin")?.Value;
+                Usr.Nome = Auth.User.FindFirst("UserNome")?.Value;
+                Usr.CodEmpresa = int.Parse(Auth.User.FindFirst("UserIdEmpresa")?.Value ?? "0");
+                Usr.Empresa = Auth.User.FindFirst("UserEmpresa")?.Value;
+
+                isAuthenticated = true;
+
+                if (Usr.CodUsuario <= 0)
+                {
+                    isAuthenticated = false;
+                    await AuthState.Logout();
+                    Navigation.NavigateTo("/");
+                }
+            }
+            else
+            {
+                isAuthenticated = false;
+                await AuthState.Logout();
+                Navigation.NavigateTo("/");
+            }
+
+            return isAuthenticated;
+        }
+    }
+}
---
+++ SGEServer/Auth/TokenService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SGEServer.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Logging;
+
+namespace SGEServer.Auth
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken(LoginModel Acessos)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim("UserId", Acessos.CodUsuario.ToString()),
+                new Claim("UserLogin", Acessos.Usuario!.ToString()),
+                new Claim("UserNome", Acessos.Nome!.ToString()),
+                new Claim("UserIdEmpresa", Acessos.CodEmpresa.ToString()),
+                new Claim("UserEmpresa", Acessos.Empresa!.ToString())
+            };
+
+            //// LISTA DE PÁGINAS QUE O USER TEM ACESSO
+            //List<int> Paginas = new List<int>();
+
+            //foreach (var item in Acessos.AcessosLista!)
+            //{
+            //    if (!Paginas.Contains(int.Parse(item.Id.ToString())))
+            //    {
+            //        Paginas.Add(int.Parse(item.Id.ToString()));
+            //        claims.Add(new Claim(ClaimTypes.Role, item.Id.ToString()));
+            //    }
+            //}
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(null,
+                null,
+                claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public IEnumerable<Claim> GetClaims(string token)
+        {
+            IdentityModelEventSource.ShowPII = true;
+
+            SecurityToken validatedToken;
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]))
+            };
+
+            ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(token, validationParameters, out validatedToken);
+
+            if (validatedToken is JwtSecurityToken jwtToken)
+            {
+                if (jwtToken.ValidTo < DateTime.UtcNow)
+                {
+                    throw new SecurityTokenExpiredException("Token expirado");
+                }
+            }
+
+            return principal.Claims;
+        }
+    }
+}
